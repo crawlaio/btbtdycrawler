565af47b600b38edaae94239417553e963be0d23
shitao <133397418@qq.com>
2019-10-05 09:45:15 +0800

    BT 电影天堂爬虫

--- btbtdy/spiders/btbtdy_movie.py
+++ btbtdy/spiders/btbtdy_movie.py
@@ -1,22 +1,34 @@
 # -*- coding: utf-8 -*-
 from urllib import parse
 
-import scrapy
+from scrapy import Request
+from scrapy_redis_sentinel.spiders import RedisSpider
 
 from btbtdy.items import BtbtdyMovieItem
 
 
-class BtbtdyMovieSpider(scrapy.Spider):
-    name = 'btbtdy_movie'
-    allowed_domains = ['btbtdy.me']
-    start_urls = ['http://www.btbtdy.me/btfl/dy1.html']
+class BtbtdyMovieSpider(RedisSpider):
+    name = "btbtdy_movie"
+    redis_key = "start_urls:{0}".format(name)
+
+    def make_request_from_data(self, data):
+        values = eval(data).get("values")
+        return Request(
+            url=eval(data).get("baseUrl"),
+            dont_filter=True,
+            meta={"data": values}
+        )
 
     def parse(self, response):
         items = BtbtdyMovieItem()
         movie_list = response.xpath("//div[@class='cts_ms']")
         for movie in movie_list:
-            items['name'] = movie.xpath("./p[@class='title']/a/@title").extract_first()
-            items['url'] = parse.urljoin(response.url, movie.xpath("./p[@class='title']/a/@href").extract_first())
-            items['score'] = movie.xpath("./p[@class='title']/span/text()").extract_first()
-            items['des'] = movie.xpath("string(./p[@class='des'])").extract_first()
+            items["name"] = movie.xpath("./p[@class='title']/a/@title").extract_first()
+            items["url"] = parse.urljoin(
+                response.url, movie.xpath("./p[@class='title']/a/@href").extract_first()
+            )
+            items["score"] = movie.xpath(
+                "./p[@class='title']/span/text()"
+            ).extract_first()
+            items["des"] = movie.xpath("string(./p[@class='des'])").extract_first()
             yield items
